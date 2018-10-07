ed58b4f57ab2bbd81db007f598f6c5f22579b017
Rajat Goyal <rajatgoyal715@gmail.com>
2018-10-07 18:12:32 +0530

    Add functions in mapper script - addItem - addPair - getPairs - setPairs - message

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log('The color is green.');
-    });
+    chrome.storage.sync.set({'map': {}}, function() {
+		console.log('Pairs set successfully');
+	});
 
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
--- js/mapper.js
+++ js/mapper.js
@@ -1,13 +1,65 @@
+var keyInput, valueInput, spanMessage, addButton;
+
 window.onload = function() {
-    var keyInput = this.document.getElementById('keyInput');
-    var valueInput = this.document.getElementById('valueInput');
+	keyInput = this.document.getElementById('keyInput');
+	valueInput = this.document.getElementById('valueInput');
 
-    var addButton = this.document.getElementById('addItem');
-    addButton.onclick = addItem;
+	addButton = this.document.getElementById('addItem');
+	addButton.onclick = addItem;
 
-    var message = this.document.getElementById('message');
-
-    function addItem() {
-        message.textContent = "New item added";
-    }
+	spanMessage = this.document.getElementById('message');
 }
+
+function addItem() {
+	var key = keyInput.value;
+	if(!key) {
+		message('Error: No key specified');
+		return;
+	}
+	var value = valueInput.value;
+	if(!value) {
+		message('Error: No value specified');
+		return;
+	}
+	addPair(key, value, function() {
+		message('New pair added successfully');
+	});
+}
+
+function addPair(key, value, callback) {
+	console.log("key:", key);
+	console.log("value:", value);
+	getPairs(function(result) {
+		pairs = result.map;
+		console.log(pairs);
+		pairs[key] = value;
+		console.log(pairs);
+		setPairs(pairs, callback);
+	});
+}
+
+function getPairs(callback) {
+	chrome.storage.sync.get(['map'], callback);
+}
+
+function setPairs(pairs, callback) {
+	chrome.storage.sync.set({'map': pairs}, callback);
+}
+
+function message(message) {
+	console.log(message);
+	spanMessage.textContent = message;
+}
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+	console.log('storage on changed');
+	for (key in changes) {
+		var storageChange = changes[key];
+		console.log('Storage key "%s" in namespace "%s" changed. ' +
+					'Old value was "%s", new value is "%s".',
+					key,
+					namespace,
+					storageChange.oldValue,
+					storageChange.newValue);
+	}
+});
