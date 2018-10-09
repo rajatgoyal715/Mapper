87435760f9eb3ad37ed3d7e481e89d035aaa193c
Ajeet Goyal <ajeet.goyal@smartprix.com>
2018-10-09 13:37:03 +0530

    no need of onCommand listener

--- js/background.js
+++ js/background.js
@@ -4,12 +4,4 @@ chrome.runtime.onInstalled.addListener(function() {
             chrome.storage.sync.set({'map': {}});
         }
     });
-	chrome.commands.onCommand.addListener(function (command) {
-		if (command === "toggle_popup") {
-			chrome.tabs.query({
-				active: true,
-				currentWindow: true
-			});
-        }
-	});
 });
